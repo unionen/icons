fff516cd546116ff169b8aa10584d632ec578c7a
Lars McLachlan <lars@mclachlan.se>
2023-11-27 11:52:02 +0100

    chore: update version

--- src/icons/status/index.ts
+++ src/icons/status/index.ts
@@ -1,17 +1,17 @@
-import CorrectMedium from "./CorrectMedium";
-import ErrorMedium from "./ErrorMedium";
-import FingerprintPersonalizedMedium from "./FingerprintPersonalizedMedium";
-import FingerprintPersonalizedSmall from "./FingerprintPersonalizedSmall";
-import InformationMedium from "./InformationMedium";
-import InformationOutlineMedium from "./InformationOutlineMedium";
-import InformationOutlineSmall from "./InformationOutlineSmall";
-import InformationSmall from "./InformationSmall";
-import LogInDoorMedium from "./LogInDoorMedium";
-import LogInKeyMedium from "./LogInKeyMedium";
-import LogInMedium from "./LogInMedium";
-import LogInSmall from "./LogInSmall";
-import QuestionMedium from "./QuestionMedium";
-import WarningMedium from "./WarningMedium";
+import CorrectMedium from './CorrectMedium';
+import ErrorMedium from './ErrorMedium';
+import FingerprintPersonalizedMedium from './FingerprintPersonalizedMedium';
+import FingerprintPersonalizedSmall from './FingerprintPersonalizedSmall';
+import InformationMedium from './InformationMedium';
+import InformationOutlineMedium from './InformationOutlineMedium';
+import InformationOutlineSmall from './InformationOutlineSmall';
+import InformationSmall from './InformationSmall';
+import LogInDoorMedium from './LogInDoorMedium';
+import LogInKeyMedium from './LogInKeyMedium';
+import LogInMedium from './LogInMedium';
+import LogInSmall from './LogInSmall';
+import QuestionMedium from './QuestionMedium';
+import WarningMedium from './WarningMedium';
 
 export {
   CorrectMedium,
